399f19f123ade2ae53d7e1a9ef482fa5ec1b05e2
Tomasl07 <104467280+Tomasl07@users.noreply.github.com>
2026-07-01 00:39:44 -0300

    Reclutador creado

--- src/batalla/Reclutador.java
+++ src/batalla/Reclutador.java
@@ -1,5 +1,41 @@
 package batalla;
 
+import personajes.Mago;
+import personajes.Auror;
+import personajes.Estudiante;
+import personajes.Profesor;
+import personajes.Seguidor;
+import personajes.Comandante;
+
+
 public class Reclutador {
-    
+    public Mago crearMago(String tipo, String nombre, int puntosVida, int nivelMagia, List<Hechizo> hechizos) {
+    	switch (tipo.toLowerCase()) {
+
+        case "auror":
+            return new Auror(nombre, puntosVida, nivelMagia, hechizos);
+
+        case "profesor":
+            return new Profesor(nombre, puntosVida, nivelMagia, hechizos);
+
+        case "estudiante":
+            return new Estudiante(nombre, puntosVida, nivelMagia, hechizos);
+
+        default:
+            throw new IllegalArgumentException("Tipo de mago inválido.");
+    	}
+    }
+    public Mortifago crearMago(String tipo, String nombre, int puntosVida, int nivelMagia, List<Hechizo> hechizos) {
+    	switch (tipo.toLowerCase()) {
+
+        case "seguidor":
+            return new Seguidor(nombre, puntosVida, nivelMagia, hechizos);
+
+        case "comandante":
+            return new Comandante(nombre, puntosVida, nivelMagia, hechizos);
+
+        default:
+            throw new IllegalArgumentException("Tipo de mago inválido.");
+    	}
+    }
 }
