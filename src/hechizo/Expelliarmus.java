21cec6122a5e5f8f1ed3cf4ec534a143b895b312
Ro~ <rocioaranibar21@gmail.com>
2026-06-30 23:55:41 -0300

    Implement Expelliarmus spell with damage effect

--- src/hechizo/Expelliarmus.java
+++ src/hechizo/Expelliarmus.java
@@ -1,5 +1,11 @@
 package hechizo;
 
-public class Expelliarmus {
+import personajes.Personaje;
 
+public class Expelliarmus implements Hechizo {
+
+    @Override
+    public void ejecutar(Personaje atacante, Personaje objetivo) {
+        objetivo.recibirDanio(20);
+    }
 }
