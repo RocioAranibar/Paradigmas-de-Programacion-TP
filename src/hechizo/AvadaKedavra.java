097d380b5ee59649118664e63daf8379aa5ca498
Ro~ <rocioaranibar21@gmail.com>
2026-06-30 23:55:08 -0300

    Implement AvadaKedavra spell execution

--- src/hechizo/AvadaKedavra.java
+++ src/hechizo/AvadaKedavra.java
@@ -1,5 +1,11 @@
 package hechizo;
 
-public class AvadaKedavra {
+import personajes.Personaje;
 
+public class AvadaKedavra implements Hechizo {
+
+    @Override
+    public void ejecutar(Personaje atacante, Personaje objetivo) {
+        objetivo.recibirDanio(50);
+    }
 }
