6a1edc0d9ade766556334dbae4f756d7641c67e8
Ro~ <rocioaranibar21@gmail.com>
2026-06-30 23:56:23 -0300

    Implement Protego class to heal attacker

--- src/hechizo/Protego.java
+++ src/hechizo/Protego.java
@@ -1,5 +1,11 @@
 package hechizo;
 
-public class Protego {
+import personajes.Personaje;
 
+public class Protego implements Hechizo {
+
+    @Override
+    public void ejecutar(Personaje atacante, Personaje objetivo) {
+        atacante.curar(15);
+    }
 }
